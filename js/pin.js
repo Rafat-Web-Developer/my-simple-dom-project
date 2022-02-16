283d24e376a06f3fd3718c1e691cd39f8e0efa68
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-16 07:49:01 +0600

    Add generate function

---
+++ js/pin.js
@@ -0,0 +1,20 @@
+
+function createPin(){
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if(pinString.length == 4){
+        return pin;
+    }else{
+        return createPin();
+    }
+}
+
+function showFieldValue(fieldId, getValue){
+    const showField = document.getElementById(fieldId);
+    showField.value = getValue;
+}
+
+function generatePin(){
+    const pin = createPin();
+    showFieldValue('showGeneratePin', pin);
+}
